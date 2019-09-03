c5648b08a95473d6be127b6a696f1f45827491d5
ANR <anrmensajera@ANRs-MacBook-Pro.local>
2019-09-03 20:39:28 -0300

    Me App made with create react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './ANRm512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Agostina Noelia Rubisse
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check out my Git!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
